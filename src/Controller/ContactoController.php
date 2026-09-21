51bb22344ef2f4f17b6d8aa706ea5433e7ccd577
ikervalls35 <ikervallsjimenez35@gmail.com>
2026-09-21 15:37:57 +0000

    Trabajo de clase dia 21/09

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -23,12 +23,7 @@ public function ficha(ManagerRegistry $doctrine, int $codigo = 1): Response
 }
 
 #[Route('/contacto/nuevo/{nombre}/{telefono}/{email}', name: 'nuevo-con-datos')]
-public function nuevoConDatos(
- ManagerRegistry $doctrine,
- string $nombre, 
- string $telefono, 
- string $email
-)
+public function nuevoConDatos(ManagerRegistry $doctrine, string $nombre, string $telefono, string $email)
 {
     $contacto = new Contacto();
     $contacto->setNombre($nombre);
@@ -39,4 +34,42 @@ public function nuevoConDatos(
     $entityManager->flush();
     return $this->redirectToRoute('contacto', ["codigo" => $contacto->getId()]);
 }
+#[Route('/contacto/update/{codigo?1}', name: 'update')]
+public function update(ManagerRegistry $doctrine, $codigo): Response
+{
+    $entityManager = $doctrine->getManager();
+    $repositorio = $doctrine->getRepository(Contacto::class);
+    $contacto = $repositorio->find($codigo);
+    $contacto->setNombre("Nombre cambiado");
+    $entityManager->persist($contacto);
+
+    try{
+        $entityManager->flush();
+        return $this->render("ficha_contacto.html.twig", ["contacto" => $contacto]);
+
+    }catch (\Exception $e){
+        return new Response("Se ha producido un error: " . $e->getMessage());
+    }
+}
+ #[Route('/contacto/borrar/{codigo}', name:'borrar')]
+    public function borrar(ManagerRegistry $doctrine, int $codigo){
+        $contacto = $doctrine->getRepository(Contacto::class)->find($codigo);
+        
+        if ($contacto){
+            $entityManager = $doctrine->getManager();
+
+            try{
+                $entityManager->remove($contacto);
+                $entityManager->flush();
+                return $this->redirectToRoute('inicio');
+
+            }catch(\Exception $e){
+                error_log('Error al eliminar el contacto: ' . $e->getMessage());
+                return new Response ("Error eliminando objeto".$e->getMessage());
+            }
+            
+        } else{
+            return new Response("No se ha encontrado el contacto");
+        }
+    }
 }
